b78e3a3ab8710f135b2ce48b2740c5ff07650970
Jacinda Chen <jrchen@dons.usfca.edu>
2020-02-10 20:00:35 -0800

    Add ebird script

---
+++ scripts/ebird.R
@@ -0,0 +1,12 @@
+# Script to read in and plot ebird data from
+# the city of San Francisco
+# Jacinda Chen
+# jrchen@dons.usfca.edu
+# February 10, 2020
+
+# load packages dplyr, ggplot2, readr to read, wrangle, and plot data
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(rebird)
+
